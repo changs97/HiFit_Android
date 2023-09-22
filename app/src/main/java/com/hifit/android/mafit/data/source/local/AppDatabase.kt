681f02e1e3071eef80bbadef04ce13c3349c96f4
qmarketchangs <changs97@aswemake.com>
2023-09-22 14:04:27 +0900

    Add: navigation action

--- app/src/main/java/com/hifit/android/mafit/data/source/local/AppDatabase.kt
+++ app/src/main/java/com/hifit/android/mafit/data/source/local/AppDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 
-@Database(entities = [UserInfo::class], version = 1, exportSchema = false)
+@Database(entities = [UserInfo::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserInfoDao
 
