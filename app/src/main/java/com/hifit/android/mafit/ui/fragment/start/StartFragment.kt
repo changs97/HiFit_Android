51ef9c17fb1c9da843cf2c1f8ec3dd92308f6ca9
qmarketchangs <changs97@aswemake.com>
2023-09-22 14:05:11 +0900

    Add: Test Logout

--- app/src/main/java/com/hifit/android/mafit/ui/fragment/start/StartFragment.kt
+++ app/src/main/java/com/hifit/android/mafit/ui/fragment/start/StartFragment.kt
@@ -2,22 +2,33 @@ package com.hifit.android.mafit.ui.fragment.start
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.hifit.android.mafit.HiFitApplication
 import com.hifit.android.mafit.R
 import com.hifit.android.mafit.base.BaseFragment
+import com.hifit.android.mafit.data.source.local.UserInfo
 import com.hifit.android.mafit.databinding.FragmentStartBinding
+import com.hifit.android.mafit.viewmodel.MainViewModel
 import com.kakao.sdk.user.UserApiClient
 import timber.log.Timber
 
 class StartFragment : BaseFragment<FragmentStartBinding>(R.layout.fragment_start) {
+    private val viewModel: MainViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        var userSurveyParticipation: UserInfo? = null
+        viewModel.userInfo.observe(viewLifecycleOwner) {
+            userSurveyParticipation = it
+        }
         binding.root.setOnClickListener {
-            HiFitApplication.sharedPreferences.getString("X_ACCESS_TOKEN", null)?.let {
-                findNavController().navigate(R.id.action_startFragment_to_homeFragment)
+            viewModel.getToken()?.let {
+                if (userSurveyParticipation != null) {
+                    findNavController().navigate(R.id.action_startFragment_to_homeFragment)
+                } else {
+                    findNavController().navigate(R.id.action_startFragment_to_surveyStartFragment)
+                }
             } ?: run {
                 findNavController().navigate(R.id.action_startFragment_to_loginFragment)
             }
@@ -35,6 +46,7 @@ class StartFragment : BaseFragment<FragmentStartBinding>(R.layout.fragment_start
                 Timber.e("연결 끊기 실패", error)
             } else {
                 Timber.i("연결 끊기 성공")
+                viewModel.deleteUserInfo()
                 activity?.finish()
             }
         }
